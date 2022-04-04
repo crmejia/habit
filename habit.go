98e3eb103b41572df16dbb584ff709b1f5d40a0a
Crismar Mejia <crismar.mejia@gmail.com>
2022-04-04 16:55:42 -0500

    adding streak logic

--- habit.go
+++ habit.go
@@ -1,19 +1,62 @@
 package habit
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Habit struct {
-	name   string
+	Name   string
 	Streak int
+	Period time.Time
 }
+type Tracker map[string]Habit
 
-func FetchHabit(name string) Habit {
-	return Habit{name: name}
+func (ht *Tracker) FetchHabit(name string) Habit {
+	habit, ok := (*ht)[name]
+	if !ok { //Create
+		habit = Habit{
+			Name:   name,
+			Period: Tomorrow(),
+		}
+		(*ht)[habit.Name] = habit
+		return habit
+	}
+
+	if SameDay(habit.Period, time.Now()) {
+		habit.Streak++
+		habit.Period = Tomorrow()
+	}
+	return habit
 }
 func (h Habit) String() string {
-	return fmt.Sprintf(messages[0], h.name)
+	var message string
+	if h.Streak == 0 {
+		message = fmt.Sprintf(NewHabit, h.Name)
+	} else if h.Streak > 0 {
+		message = fmt.Sprintf(StreakHabit, h.Name, h.Streak)
+	}
+	return message
 }
 
-var messages = map[int]string{
-	0: "Good luck with your new habit '%s'! Don't forget to do it again\ntomorrow.",
+const (
+	NewHabit    = "Good luck with your new habit '%s'! Don't forget to do it again tomorrow."
+	StreakHabit = "Nice work: you've done the habit '%s' for %d days in a row now. Keep it up!"
+)
+
+//var messages = map[string]string{
+//	"newHabit": "Good luck with your new habit '%s'! Don't forget to do it again\ntomorrow.",
+//	1:          "Nice work: you've done the habit '%s' for %s days in a row now.\nKeep it up!",
+//}
+
+func Tomorrow() time.Time {
+	return time.Now().Add(24 * time.Hour)
+}
+
+//func Same Day returns true if the days are the same ignoring hours, mins,etc
+func SameDay(d1, d2 time.Time) bool {
+	if d1.Year() == d2.Year() && d1.Month() == d2.Month() && d1.Day() == d2.Day() {
+		return true
+	}
+	return false
 }
--- habit_test.go
+++ habit_test.go
@@ -3,24 +3,64 @@ package habit_test
 import (
 	"habit"
 	"testing"
+	"time"
 )
 
 func TestHabitImplementsStringer(t *testing.T) {
-	want := "Good luck with your new habit 'piano'! Don't forget to do it again\ntomorrow."
-	h := habit.FetchHabit("piano")
-	got := h.String()
-
-	if want != got {
-		t.Errorf("For day 0: want the message to be %s,\n got %s", want, got)
+	testCases := []struct {
+		want  string
+		habit habit.Habit
+	}{
+		{want: "Good luck with your new habit 'piano'! Don't forget to do it again tomorrow.", habit: habit.Habit{Name: "piano", Streak: 0}},
+		{want: "Nice work: you've done the habit 'surf' for 4 days in a row now. Keep it up!", habit: habit.Habit{Name: "surf", Streak: 4}},
+	}
+	for _, tc := range testCases {
+		got := tc.habit.String()
+		if tc.want != got {
+			t.Errorf("For %d day streak: want the message to be %s,\n got %s", tc.habit.Streak, tc.want, got)
+		}
 	}
 }
 
 func TestFetchHabitReturnsANewHabitWithZeroDaysStreakOnNewHabit(t *testing.T) {
-	h := habit.FetchHabit("piano")
+	tracker := habit.Tracker{}
+	h := tracker.FetchHabit("piano")
 	want := 0
 	got := h.Streak
 
 	if want != got {
 		t.Errorf("For a new habit want %d,\n got %d", want, got)
+	}
+}
+
+func TestFetchHabitIncreasesStreakOnExistingHabit(t *testing.T) {
+	tracker := habit.Tracker{
+		"piano": habit.Habit{
+			Name:   "piano",
+			Streak: 1,
+			Period: time.Now(),
+		},
+	}
+	h := tracker.FetchHabit("piano")
+	want := 2
+	got := h.Streak
+	if want != got {
+		t.Errorf("want streak to increase to %d, got %d", want, got)
+	}
+}
+
+func TestFetchHabitIncreaseStreakOncePerDay(t *testing.T) {
+	tracker := habit.Tracker{
+		"piano": habit.Habit{
+			Name:   "piano",
+			Streak: 1,
+			Period: habit.Tomorrow(),
+		},
+	}
+	h := tracker.FetchHabit("piano")
+	want := 1
+	got := h.Streak
+	if want != got {
+		t.Errorf("want streak to increase to %d, got %d", want, got)
 	}
 }
